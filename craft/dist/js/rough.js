0482b3e5c657610216853b7b7278df8691b75c95
S4F4Y4T <safayatmahmud6969@gmail.com>
2022-10-29 12:38:46 +0600

    removed craft

--- craft/dist/js/rough.js
+++
@@ -1,5 +0,0 @@
-// import { annotate } from 'https://unpkg.com/rough-notation?module';
-
-// const e = document.querySelector('.nav__link');
-// const annotation = annotate(e, { type: 'underline' });
-// annotation.show();
